b4bdc1236568e8c7fed6de27d2e768d974aeea29
Ian Wallace <167253792+14N-W4774c3@users.noreply.github.com>
2024-11-08 22:47:44 +0000

    Step 1 Complete - Added button.

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const spookyButton = document.createElement("button");
+spookyButton.textContent = "👻";
+document.body.append(spookyButton);
