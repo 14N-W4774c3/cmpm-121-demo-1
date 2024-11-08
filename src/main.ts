a88e71fa787302f39e3ffd75b8fb512923096ae4
Ian Wallace <167253792+14N-W4774c3@users.noreply.github.com>
2024-11-08 22:54:44 +0000

    Step 2 Complete - Added haunting counter.  Implemented haunting increase on button press.

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,20 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+const spookyDiv = document.createElement("div");
+document.body.append(spookyDiv);
+
+const spookyCounter = document.createElement("p");
+spookyCounter.textContent = "Hauntings: 0";
+spookyDiv.append(spookyCounter);
+
+let ghosts = 0;
+
 const spookyButton = document.createElement("button");
 spookyButton.textContent = "👻";
 document.body.append(spookyButton);
+
+spookyButton.onclick = () => {
+  ghosts++;
+  spookyCounter.textContent = `Hauntings: ${ghosts}`;
+};
